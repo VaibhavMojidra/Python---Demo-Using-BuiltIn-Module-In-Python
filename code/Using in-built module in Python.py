1587f68da38c1b59c10a304e02638941a1a9cf86
VaibhavMojidra <vmojidra25@gmail.com>
2022-09-21 00:23:14 +0530

    Using BuiltIn Module In Python

---
+++ code/Using in-built module in Python.py
@@ -0,0 +1,16 @@
+#Using in-built module in Python
+
+import random
+
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+print(random.randint(1,100))
+
+import datetime
+
+print(datetime.datetime.now())
+print(datetime.datetime.now().year)
+print(datetime.datetime.now().day)
+
